39d18d0303337dc913b03f3280a2d8bab2b248ba
clement <garrigou@free.fr>
2022-05-10 22:49:24 +0200

    Gestion des personnes

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use App\Repository\PersonneRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,9 +13,12 @@ class MainController extends AbstractController
     /**
     * @Route("/")
     */
-    public function index(): Response
+    public function index(PersonneRepository $personneRepository): Response
     {
+        $personnes = $personneRepository->findAll();
+
         return $this->render('main/index.html.twig', [
+            'personnes' => $personnes
         ]);
     }
 }
